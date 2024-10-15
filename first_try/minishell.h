6ea9332aa19ea9b7dfae5366e02deca394a44f21
Aina Rajaonah Andriamanalimanda <aandriam@c1r6s15.42antananarivo.mg>
2024-10-15 16:11:40 +0300

    ajout auto

--- first_try/minishell.h
+++ first_try/minishell.h
@@ -6,13 +6,14 @@
 /*   By: aandriam <aandriam@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 11:33:19 by aandriam          #+#    #+#             */
-/*   Updated: 2024/10/11 11:00:31 by aandriam         ###   ########.fr       */
+/*   Updated: 2024/10/15 16:07:47 by aandriam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "lib/lib.h"
 # include <fcntl.h>
 # include <readline/history.h>
 # include <readline/readline.h>
@@ -55,21 +56,11 @@ typedef struct s_vars
 	struct s_pipe			*pipe;
 }							t_vars;
 
-void						ft_putstr_fd(char *s, int fd);
-char						*ft_strrchr(const char *s, int c);
-int							ft_strncmp(const char *s1, const char *s2);
 int							number_of_line(int fd);
-void						ft_putchar_fd(char c, int fd);
-void						ft_putnbr_fd(int n, int fd);
-size_t						ft_strlen(const char *s);
 char						*join_them(char *new_buff, char *temp);
 char						have_nl(char *buffer);
-char						*ft_substr(char const *s, unsigned int start,
-								size_t len);
 char						*only_readable(char *buffer);
 void						ft_add_history(char *input, t_vars *vars);
-char						*ft_strjoin(char const *s1, char const *s2);
-char						*ft_strdup(const char *s);
 void						big_param_init(char ***big_param, t_vars vars);
 void						terminate_shell_init(char **big_param);
 void						vars_init(t_vars *vars, char **env);
--- first_try/simple_execution/cmd_test/redir_test/s_o_test/test_exec_redir/test_exec_redir.c
+++ first_try/simple_execution/cmd_test/redir_test/s_o_test/test_exec_redir/test_exec_redir.c
@@ -6,7 +6,7 @@
 /*   By: aandriam <aandriam@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 16:00:42 by aandriam          #+#    #+#             */
-/*   Updated: 2024/10/14 15:26:33 by aandriam         ###   ########.fr       */
+/*   Updated: 2024/10/14 16:56:09 by aandriam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,8 @@
 
 void	input_redir_test(t_pipe *lol, char *log_dir)
 {
-	char	*dir;
-	char	*lol_dir;
-	char	*tmp;
-	char	*input;
-
-	lol_dir = lol->redirection->next->cmd->cmd;
-	tmp = ft_strjoin(log_dir, "/");
-	dir = ft_strjoin(tmp, lol_dir);
-	free(tmp);
-	free(lol_dir);
 	if (file_exist(dir))
 	{
-		input = take_input(dir);
-		ft_putstr_fd("the content of the file is ", 1);
-		ft_putstr_fd("\n", 1);
-		ft_putstr_fd(input, 1);
 	}
 	else
 	{
