e38e104517d17daa0d827a2db1bf7a0d746b42b2
Aina Rajaonah Andriamanalimanda <aandriam@c1r5s9.42antananarivo.mg>
2024-11-08 15:50:18 +0300

    ajout auto

--- aandriam/second_try/simple_execution/redirection_test/first_test/exec_t_pipe/no_pipe/ft_cd_chill.c
+++ aandriam/second_try/simple_execution/redirection_test/first_test/exec_t_pipe/no_pipe/ft_cd_chill.c
@@ -6,29 +6,29 @@
 /*   By: aandriam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 10:45:56 by aandriam          #+#    #+#             */
-/*   Updated: 2024/11/08 10:59:35 by aandriam         ###   ########.fr       */
+/*   Updated: 2024/11/08 14:28:58 by aandriam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../first_test.h"
 
-static int	error_check(char **args)
+static int	error_check(char *args)
 {
 	struct stat	path_stat;
 
-	if (stat(args[1], &path_stat) == -1)
+	if (stat(args, &path_stat) == -1)
 	{
-		ft_perror_fork(args[1], "cd: No such file or directory\n");
+		ft_perror_fork(args, "cd: No such file or directory\n");
 		return (0);
 	}
 	if (!S_ISDIR(path_stat.st_mode))
 	{
-		ft_perror_fork(args[1], "cd: Not a directory\n");
+		ft_perror_fork(args, "cd: Not a directory\n");
 		return (0);
 	}
-	if (access(args[1], X_OK) == -1)
+	if (access(args, X_OK) == -1)
 	{
-		ft_perror_fork(args[1], "cd: Permission denied\n");
+		ft_perror_fork(args, "cd: Permission denied\n");
 		return (0);
 	}
 	return (1);
@@ -44,6 +44,17 @@ static void	update_oldpwd_env(char *dir)
 	(void)dir;
 }
 
+static void	go_home(void)
+{
+	char	*home_dir;
+
+	home_dir = getenv("HOME");
+	if (!error_check(home_dir))
+		return ;
+	if (chdir(home_dir) == -1)
+		ft_perror_fork(home_dir, "cd: Failed to change directory\n");
+}
+
 void	ft_cd_chill(t_command *cmd)
 {
 	int		i;
@@ -53,12 +64,14 @@ void	ft_cd_chill(t_command *cmd)
 	i = 0;
 	while (tab[i])
 		i++;
-	if (i > 2)
+	if (i == 1)
+		go_home();
+	else if (i > 2)
 	{
 		ft_perror_fork("cd", "too many arguments\n");
 		return ;
 	}
-	if (!error_check(tab))
+	if (!error_check(tab[1]))
 		return ;
 	if (chdir(tab[1]) == -1)
 	{
