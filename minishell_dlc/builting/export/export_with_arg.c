77c5821b83dbbba0915b3b4990ecba59e33cd9bb
Aina Rajaonah Andriamanalimanda <aandriam@c1r11s9.42antananarivo.mg>
2025-01-05 17:06:45 +0300

    bonbon sucette

--- minishell_dlc/builting/export/export_with_arg.c
+++ minishell_dlc/builting/export/export_with_arg.c
@@ -6,7 +6,7 @@
 /*   By: mravelon <mravelon@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 14:02:31 by mravelon          #+#    #+#             */
-/*   Updated: 2025/01/05 15:49:18 by aandriam         ###   ########.fr       */
+/*   Updated: 2025/01/05 17:04:15 by aandriam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,6 @@
 
 void	export_with_arg(t_list **env, char *arg)
 {
-	char	**splited_arg;
-	int		i;
-
-	i = 0;
-	splited_arg = simple_split(arg, ' ');
-	while (splited_arg[i])
-	{
-		if (check_list(env, splited_arg[i]) == 1)
-			creat_chain_of_list(env, creat_bloc_of_list(splited_arg[i]));
-		i++;
-	}
-	ft_free_tab(&splited_arg);
+		if (check_list(env, arg) == 1)
+			creat_chain_of_list(env, creat_bloc_of_list(arg));
 }
